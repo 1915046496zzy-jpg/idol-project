5c8d850a4a5d176408be681a818bbb525c89bfbb
mosquito <1915046496zzy@gmail.com>
2026-06-07 18:32:01 +0800

    Create assets_map.js

---
+++ assets_map.js
@@ -0,0 +1,14 @@
+// 剧情插图、场景、表情包映射表
+var AssetsMap = {
+    // 【表情包区】
+    "表情_开心": "https://i.postimg.cc/xxx/smile.png",
+    "表情_生气": "https://i.postimg.cc/xxx/angry.png",
+
+    // 【剧情插图区】
+    "加爱_排练室汗水": "https://i.postimg.cc/MGf2Z4Bp/qian-gong-jia-ai.png",
+    "希未_后台睡觉": "https://i.postimg.cc/wxsZw3L9/lu-tian-xi-wei-zhun-bei-deng-tai.png",
+
+    // 【场景地图区】
+    "场景_1030排练室": "https://i.postimg.cc/xxx/room.jpg",
+    "场景_高级酒店": "https://i.postimg.cc/MKKXk7zW/hotel.jpg"
+};
