12ae2f9c940e109dc853b2748a58b78c98dcb37d
mosquito <1915046496zzy@gmail.com>
2026-06-09 21:06:30 +0800

    Update game_config.js

--- game_config.js
+++ game_config.js
@@ -82,12 +82,4 @@ var itemPool = {
 };
 
 
-// ====== 系统主题配置 ======
-// 之后如果想增加新主题（比如樱花季、万圣节），在这里添加就行
-var systemThemes = {
-    'spring': { class: 'bg-spring', main: '#db2777', particleType: 'sakura' },
-    'summer': { class: 'bg-summer', main: '#16a34a', particleType: 'summer-leaf' },
-    'autumn': { class: 'bg-autumn', main: '#d97706', particleType: 'leaf' },
-    'winter': { class: 'bg-winter', main: '#0369a1', particleType: 'snow' },
-    'none': { class: '', main: '#64748b', particleType: 'none' }
-};
+
